aba100fd62ffa3e5f1a5459c2c3ca6436b7e2d67
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-08 11:08:31 +0300

    Typo fix in base test

--- test/BaseNotification.js
+++ test/BaseNotification.js
@@ -1,31 +1,33 @@
 var assert = require('chai').assert;
-var BasePushNotification = require('../lib/BaseNotification');
+var BaseNotification = require('../lib/BaseNotification');
 
 describe('BaseNotification', function () {
   it('Should properly export', function () {
-    assert.isFunction(BasePushNotification);
-    assert.isFunction(BasePushNotification.prototype.getConfig);
-    assert.isFunction(BasePushNotification.prototype.setConfig);
-    assert.isFunction(BasePushNotification.prototype.send);
+    assert.isFunction(BaseNotification);
+    assert.isFunction(BaseNotification.prototype.getConfig);
+    assert.isFunction(BaseNotification.prototype.setConfig);
+    assert.isFunction(BaseNotification.prototype.getProvider);
+    assert.isFunction(BaseNotification.prototype.setProvider);
+    assert.isFunction(BaseNotification.prototype.send);
 
     assert.throw(function () {
-      BasePushNotification.prototype.send();
+      BaseNotification.prototype.send();
     }, Error);
   });
 
   it('Should properly make objects configurable', function () {
-    var pusher = new BasePushNotification();
+    var pusher = new BaseNotification();
 
     assert.notOk(pusher.getConfig('foo'));
-    assert.instanceOf(pusher.setConfig('foo', 'bar'), BasePushNotification);
-    assert.instanceOf(pusher.setConfig('obj', {foo: 'bar'}), BasePushNotification);
+    assert.instanceOf(pusher.setConfig('foo', 'bar'), BaseNotification);
+    assert.instanceOf(pusher.setConfig('obj', {foo: 'bar'}), BaseNotification);
     assert.deepEqual(pusher.getConfig('obj'), {foo: 'bar'});
     assert.equal(pusher.getConfig('obj').foo, 'bar');
     assert.equal(pusher.getConfig('foo'), 'bar');
   });
 
   it('Should properly create pusher with pre-defined config', function () {
-    var pusher = new BasePushNotification({
+    var pusher = new BaseNotification({
       foo: 'bar'
     });
 
@@ -34,15 +36,15 @@ describe('BaseNotification', function () {
   });
 
   it('Should properly get/set provider', function () {
-    var pusher = new BasePushNotification();
+    var pusher = new BaseNotification();
 
     assert.notOk(pusher.getProvider());
-    assert.instanceOf(pusher.setProvider('SOME_PROVIDER'), BasePushNotification);
+    assert.instanceOf(pusher.setProvider('SOME_PROVIDER'), BaseNotification);
     assert.equal(pusher.getProvider(), 'SOME_PROVIDER');
   });
 
   it('Should properly throw error on send', function () {
-    var pusher = new BasePushNotification();
+    var pusher = new BaseNotification();
 
     assert.throw(function () {
       pusher.send();
