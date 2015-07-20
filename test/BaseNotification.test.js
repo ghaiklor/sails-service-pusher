8d7c9fc3dc2b71c5ddf78529c7ba6689eab70e7f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-20 14:14:47 +0300

    Rewrite tests for BaseNotification

--- test/BaseNotification.test.js
+++ test/BaseNotification.test.js
@@ -4,50 +4,43 @@ var BaseNotification = require('../lib/BaseNotification');
 describe('BaseNotification', function () {
   it('Should properly export', function () {
     assert.isFunction(BaseNotification);
-    assert.isFunction(BaseNotification.prototype.getConfig);
-    assert.isFunction(BaseNotification.prototype.setConfig);
+    assert.isFunction(BaseNotification.prototype.get);
+    assert.isFunction(BaseNotification.prototype.set);
     assert.isFunction(BaseNotification.prototype.getProvider);
     assert.isFunction(BaseNotification.prototype.setProvider);
     assert.isFunction(BaseNotification.prototype.send);
-
-    assert.throw(function () {
-      BaseNotification.prototype.send();
-    }, Error);
   });
 
   it('Should properly make objects configurable', function () {
-    var pusher = new BaseNotification();
+    var notification = new BaseNotification();
 
-    assert.notOk(pusher.getConfig('foo'));
-    assert.instanceOf(pusher.setConfig('foo', 'bar'), BaseNotification);
-    assert.instanceOf(pusher.setConfig('obj', {foo: 'bar'}), BaseNotification);
-    assert.deepEqual(pusher.getConfig('obj'), {foo: 'bar'});
-    assert.equal(pusher.getConfig('obj').foo, 'bar');
-    assert.equal(pusher.getConfig('foo'), 'bar');
+    assert.notOk(notification.get('foo'));
+    assert.instanceOf(notification.set('foo', 'bar'), BaseNotification);
+    assert.instanceOf(notification.set('obj', {foo: 'bar'}), BaseNotification);
+    assert.deepEqual(notification.get('obj'), {foo: 'bar'});
+    assert.equal(notification.get('obj.foo'), 'bar');
+    assert.equal(notification.get('foo'), 'bar');
   });
 
-  it('Should properly create pusher with pre-defined config', function () {
-    var pusher = new BaseNotification({
-      foo: 'bar'
+  it('Should properly create notification with pre-defined config', function () {
+    var notification = new BaseNotification({
+      foo: 'bar',
+      obj: {
+        foo: 'bar'
+      }
     });
 
-    assert.equal(pusher.getConfig('foo'), 'bar');
-    assert.notOk(pusher.getConfig('NOT_EXISTS'));
+    assert.equal(notification.get('foo'), 'bar');
+    assert.equal(notification.get('obj.foo'), 'bar');
+    assert.deepEqual(notification.get('obj'), {foo: 'bar'});
+    assert.notOk(notification.get('NOT_EXISTS'));
   });
 
   it('Should properly get/set provider', function () {
-    var pusher = new BaseNotification();
+    var notification = new BaseNotification();
 
-    assert.notOk(pusher.getProvider());
-    assert.instanceOf(pusher.setProvider('SOME_PROVIDER'), BaseNotification);
-    assert.equal(pusher.getProvider(), 'SOME_PROVIDER');
-  });
-
-  it('Should properly throw error on send', function () {
-    var pusher = new BaseNotification();
-
-    assert.throw(function () {
-      pusher.send();
-    }, Error);
+    assert.notOk(notification.getProvider());
+    assert.instanceOf(notification.setProvider('NOTIFICATION'), BaseNotification);
+    assert.equal(notification.getProvider(), 'NOTIFICATION');
   });
 });
