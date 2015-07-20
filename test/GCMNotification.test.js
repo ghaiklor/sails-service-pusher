7ddc8af340194dc51e1f9805c8320beffefd396f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-20 17:14:27 +0300

    Improve test coverage for GCM

--- test/GCMNotification.test.js
+++ test/GCMNotification.test.js
@@ -2,24 +2,31 @@ var assert = require('chai').assert;
 var sinon = require('sinon');
 var GCMNotification = require('../lib/GCMNotification');
 
-var PROVIDER_CONFIG = {
+var CONFIG = {
+  device: ['a1'],
   provider: {
     apiKey: 'test'
   },
   notification: {
     title: 'TITLE',
+    body: 'BODY',
     icon: 'ICON',
-    badge: 1
+    sound: 'SOUND',
+    badge: 'BADGE',
+    payload: {
+      foo: 'bar',
+      bar: 'foo'
+    }
   }
 };
 
-var DEVICES_LIST = ['a1', 'b2', 'c3'];
 var NOTIFICATION_SHOULD_BE = {
   notification: {
     title: 'TITLE',
-    icon: 'ICON',
     body: 'BODY',
-    badge: 1
+    icon: 'ICON',
+    sound: 'SOUND',
+    badge: 'BADGE'
   },
   data: {
     foo: 'bar',
@@ -32,47 +39,190 @@ describe('GCMNotification', function () {
     assert.isFunction(GCMNotification);
   });
 
-  it('Should properly send notification to multiple devices', function () {
-    var android = new GCMNotification(PROVIDER_CONFIG);
+  it('Should properly send notification with pre-defined options', function (done) {
+    var android = new GCMNotification(CONFIG);
 
-    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
-      cb();
+    sinon.stub(android.getProvider(), 'send', function (message, devices, cb) {
+      cb(null, 'RESULT');
     });
 
-    android.send(DEVICES_LIST, {
-      body: 'BODY',
-      payload: {
-        foo: 'bar',
-        bar: 'foo'
-      }
-    });
+    android
+      .send(['b2', 'c3'])
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert(android.getProvider().send.calledOnce);
+        assert.deepEqual(android.getProvider().send.getCall(0).args[0].params, NOTIFICATION_SHOULD_BE);
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1', 'b2', 'c3']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
 
-    assert(android.getProvider().send.calledOnce);
-    assert.deepEqual(android.getProvider().send.getCall(0).args[0].params, NOTIFICATION_SHOULD_BE);
-    assert.equal(android.getProvider().send.getCall(0).args[1], DEVICES_LIST);
+        android.getProvider().send.restore();
 
-    android.getProvider().send.restore();
+        done();
+      })
+      .catch(done);
   });
 
-  it('Should properly send notification to one device', function () {
-    var android = new GCMNotification(PROVIDER_CONFIG);
+  it('Should properly send notification with custom notification', function (done) {
+    var android = new GCMNotification(CONFIG);
 
     sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
-      cb();
+      cb(null, 'RESULT');
     });
 
-    android.send('a1', {
-      body: 'BODY',
-      payload: {
-        foo: 'bar',
-        bar: 'foo'
-      }
+    android
+      .send(['b2', 'c3'], {
+        body: 'OVERRIDE_BODY'
+      })
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', 'OVERRIDE_BODY');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', 'TITLE');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.data.foo', 'bar');
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1', 'b2', 'c3']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
+
+        android.getProvider().send.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly send notification with extended notification', function (done) {
+    var android = new GCMNotification(CONFIG);
+
+    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
+      cb(null, 'RESULT');
     });
 
-    assert(android.getProvider().send.calledOnce);
-    assert.deepEqual(android.getProvider().send.getCall(0).args[0].params, NOTIFICATION_SHOULD_BE);
-    assert.equal(android.getProvider().send.getCall(0).args[1], 'a1');
+    android
+      .send(['b2'], {
+        body: 'OVERRIDE_BODY'
+      }, {
+        dryRun: true
+      })
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert.ok(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', 'OVERRIDE_BODY');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', 'TITLE');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.dryRun', true);
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1', 'b2']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
 
-    android.getProvider().send.restore();
+        android.getProvider().send.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly send notification with all empty config', function (done) {
+    var android = new GCMNotification();
+
+    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
+      cb(null, 'RESULT');
+    });
+
+    android
+      .send()
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert.ok(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', '');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', '');
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], []);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
+
+        android.getProvider().send.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly send notification with empty pre-defined config and empty notification', function (done) {
+    var android = new GCMNotification();
+
+    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
+      cb(null, 'RESULT');
+    });
+
+    android
+      .send(['a1'])
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert.ok(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', '');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', '');
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
+
+        android.getProvider().send.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly send notification with empty pre-defined config and custom notification', function (done) {
+    var android = new GCMNotification();
+
+    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
+      cb(null, 'RESULT');
+    });
+
+    android
+      .send(['a1'], {
+        title: 'CUSTOM_TITLE',
+        body: 'CUSTOM_BODY'
+      }, {
+        dryRun: true
+      })
+      .then(function (result) {
+        assert.equal(result, 'RESULT');
+        assert.ok(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', 'CUSTOM_TITLE');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', 'CUSTOM_BODY');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.dryRun', true);
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
+
+        android.getProvider().send.restore();
+
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly reject promise on send notification', function (done) {
+    var android = new GCMNotification();
+
+    sinon.stub(android.getProvider(), 'send', function (message, device, cb) {
+      cb(new Error('Some error occurred'));
+    });
+
+    android
+      .send(['a1'], {
+        title: 'CUSTOM_TITLE',
+        body: 'CUSTOM_BODY'
+      }, {
+        dryRun: true
+      })
+      .then(done)
+      .catch(function (error) {
+        assert.instanceOf(error, Error);
+        assert.ok(android.getProvider().send.calledOnce);
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.title', 'CUSTOM_TITLE');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.notification.body', 'CUSTOM_BODY');
+        assert.deepPropertyVal(android.getProvider().send.getCall(0).args[0], 'params.dryRun', true);
+        assert.deepEqual(android.getProvider().send.getCall(0).args[1], ['a1']);
+        assert.isFunction(android.getProvider().send.getCall(0).args[2]);
+
+        android.getProvider().send.restore();
+
+        done();
+      });
   });
 });
