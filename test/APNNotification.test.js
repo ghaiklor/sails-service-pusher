a6d2b03eab1d0584d5cbc96eac0fbdb95acda83e
Eugene Obrezkov <ghaiklor@gmail.com>
2015-07-20 18:01:08 +0300

    Add Promise.resolve() to apn.send

--- test/APNNotification.test.js
+++ test/APNNotification.test.js
@@ -42,106 +42,135 @@ describe('APNNotification', function () {
     assert.isFunction(APNNotification);
   });
 
-  it('Should properly send notification with pre-defined options', function () {
+  it('Should properly send notification with pre-defined options', function (done) {
     var ios = new APNNotification(CONFIG);
 
     sinon.stub(ios.getProvider(), 'pushNotification');
 
-    ios.send(['b2', 'c3']);
+    ios
+      .send(['b2', 'c3'])
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.calledThrice);
+        assert.deepEqual(ios.getProvider().pushNotification.getCall(0).args[0].payload, NOTIFICATION_SHOULD_BE);
+        assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
+        assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
+        assert.equal(ios.getProvider().pushNotification.getCall(2).args[1], 'c3');
 
-    assert.ok(ios.getProvider().pushNotification.calledThrice);
-    assert.deepEqual(ios.getProvider().pushNotification.getCall(0).args[0].payload, NOTIFICATION_SHOULD_BE);
-    assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
-    assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
-    assert.equal(ios.getProvider().pushNotification.getCall(2).args[1], 'c3');
+        ios.getProvider().pushNotification.restore();
 
-    ios.getProvider().pushNotification.restore();
+        done();
+      })
+      .catch(done);
   });
 
-  it('Should properly send notification with custom notification', function () {
+  it('Should properly send notification with custom notification', function (done) {
     var ios = new APNNotification(CONFIG);
 
     sinon.stub(ios.getProvider(), 'pushNotification');
 
-    ios.send(['b2', 'c3'], {
-      body: 'OVERRIDE_BODY'
-    });
+    ios
+      .send(['b2', 'c3'], {
+        body: 'OVERRIDE_BODY'
+      })
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.calledThrice);
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'OVERRIDE_BODY');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.sound', 'SOUND');
+        assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
+        assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
+        assert.equal(ios.getProvider().pushNotification.getCall(2).args[1], 'c3');
 
-    assert.ok(ios.getProvider().pushNotification.calledThrice);
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'OVERRIDE_BODY');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.sound', 'SOUND');
-    assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
-    assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
-    assert.equal(ios.getProvider().pushNotification.getCall(2).args[1], 'c3');
+        ios.getProvider().pushNotification.restore();
 
-    ios.getProvider().pushNotification.restore();
+        done();
+      })
+      .catch(done);
   });
 
-  it('Should properly send notification with extended notification', function () {
+  it('Should properly send notification with extended notification', function (done) {
     var ios = new APNNotification(CONFIG);
 
     sinon.stub(ios.getProvider(), 'pushNotification');
 
-    ios.send(['b2'], {
-      body: 'OVERRIDE_BODY'
-    }, {
-      priority: 5
-    });
+    ios
+      .send(['b2'], {
+        body: 'OVERRIDE_BODY'
+      }, {
+        priority: 5
+      })
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.calledTwice);
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'OVERRIDE_BODY');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 5);
+        assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
+        assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
 
-    assert.ok(ios.getProvider().pushNotification.calledTwice);
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'OVERRIDE_BODY');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 5);
-    assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
-    assert.equal(ios.getProvider().pushNotification.getCall(1).args[1], 'b2');
+        ios.getProvider().pushNotification.restore();
 
-    ios.getProvider().pushNotification.restore();
+        done();
+      })
+      .catch(done);
   });
 
-  it('Should properly send notification with all empty config', function () {
+  it('Should properly send notification with all empty config', function (done) {
     var ios = new APNNotification();
     sinon.stub(ios.getProvider(), 'pushNotification');
 
-    ios.send();
+    ios
+      .send()
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.notCalled);
+        ios.getProvider().pushNotification.restore();
 
-    assert.ok(ios.getProvider().pushNotification.notCalled);
-
-    ios.getProvider().pushNotification.restore();
+        done();
+      })
+      .catch(done);
   });
 
-  it('Should properly send notification with empty pre-defined config and empty notification', function () {
-    var ios = new APNNotification();
-
-    sinon.stub(ios.getProvider(), 'pushNotification');
-
-    ios.send(['a1']);
-
-    assert.ok(ios.getProvider().pushNotification.calledOnce);
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.title', '');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', '');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 10);
-    assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
-
-    ios.getProvider().pushNotification.restore();
-  });
-
-  it('Should properly send notification with empty pre-defined config and custom notification', function () {
+  it('Should properly send notification with empty pre-defined config and empty notification', function (done) {
     var ios = new APNNotification();
 
     sinon.stub(ios.getProvider(), 'pushNotification');
 
-    ios.send(['a1'], {
-      title: 'CUSTOM_TITLE',
-      body: 'CUSTOM_BODY'
-    }, {
-      priority: 5
-    });
+    ios
+      .send(['a1'])
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.calledOnce);
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.title', '');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', '');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 10);
+        assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
 
-    assert.ok(ios.getProvider().pushNotification.calledOnce);
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.title', 'CUSTOM_TITLE');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'CUSTOM_BODY');
-    assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 5);
-    assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
+        ios.getProvider().pushNotification.restore();
 
-    ios.getProvider().pushNotification.restore();
+        done();
+      })
+      .catch(done);
+  });
+
+  it('Should properly send notification with empty pre-defined config and custom notification', function (done) {
+    var ios = new APNNotification();
+
+    sinon.stub(ios.getProvider(), 'pushNotification');
+
+    ios
+      .send(['a1'], {
+        title: 'CUSTOM_TITLE',
+        body: 'CUSTOM_BODY'
+      }, {
+        priority: 5
+      })
+      .then(function () {
+        assert.ok(ios.getProvider().pushNotification.calledOnce);
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.title', 'CUSTOM_TITLE');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'payload.aps.alert.body', 'CUSTOM_BODY');
+        assert.deepPropertyVal(ios.getProvider().pushNotification.getCall(0).args[0], 'priority', 5);
+        assert.equal(ios.getProvider().pushNotification.getCall(0).args[1], 'a1');
+
+        ios.getProvider().pushNotification.restore();
+
+        done();
+      })
+      .catch(done);
   });
 });
